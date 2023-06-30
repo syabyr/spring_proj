548db119e9abcf29d4ae9e42dbf33a35792e431f
syabyr <mybays@sina.com>
2023-06-30 23:15:43 +0800

    test

--- helloworld/src/main/java/com/mybays/helloworld/MyApplication.java
+++ helloworld/src/main/java/com/mybays/helloworld/MyApplication.java
@@ -1,12 +1,14 @@
 package com.mybays.helloworld;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableAutoConfiguration
+//@EnableAutoConfiguration
+@SpringBootApplication
 public class MyApplication {
 
     @RequestMapping("/")
